903a3cef0fe0e03b39a05dbc5759688fef77b00b
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2023-10-23 17:10:19 +0530

    final commit

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="bg-white rounded-full border-t-2 border-r-2 border-black h-10 w-10 animate-spin" />
+    </div>
+  );
 };
 
 export default Loader;
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 import { BsThreeDotsVertical, BsBookmark } from "react-icons/bs";
 import Button from "@mui/material/Button";
-const Result = ({ type }) => {
+import { Link } from "react-router-dom";
+const Result = ({ type, apiResponse }) => {
   return (
     <div className="w-full px-5 sm:px-10 my-5 mb-20 sm:mt-10 gap-2 sm:gap-4 flex flex-col">
       <p className="text-2xl">{type} Result</p>
       <div className="flex flex-col gap-4 sm:gap-8">
-        <Data type={type} />
-        <Data type={type} />
+        {apiResponse === undefined ? (
+          <div className=" flex items-center italic tracking-widest flex-col h-[30vh] text-red-400 justify-center text-base sm:text-xl md:text-3xl">
+            <p>No Result</p>
+            <p>Or Some Error Occured</p>
+            <p className="not-italic text-xs sm:text-base tracking-widest font-extralight">
+              Reload Page
+            </p>
+          </div>
+        ) : (
+          apiResponse?.map((item, index) => {
+            return (
+              <div key={index}>
+                <Data ind={index} type={type} item={item} />;
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
 };
-const Data = ({ type }) => {
+const Data = ({ type, item, ind }) => {
+  // console.log("data api response", item);
+  // console.log(item.url);
   return (
-    <div className="flex flex-col gap-4 px-4 py-5 rounded-xl drop-shadow-sm border-t-2 border-gray-50 shadow-gray-300 shadow-md">
-      <div className="flex justify-end sm:justify-between gap-2">
-        <p className="hidden sm:block text-gray-400">
-          www.nyaylok.onrender.com
-        </p>
+    <div
+      key={ind}
+      className="flex flex-col gap-4 px-4 py-5 rounded-xl drop-shadow-sm border-t-2 border-gray-50 shadow-gray-300 shadow-md"
+    >
+      <div className="flex justify-end md:justify-between gap-2">
+        <p className="hidden md:block text-xs  text-gray-400">{item?.url}</p>
         <div className="flex gap-2 items-center text-green-400 font-semibold text-lg">
           <Button color="success" className="flex gap-2">
             <BsBookmark />
@@ -28,17 +47,13 @@ const Data = ({ type }) => {
         </div>
       </div>
       <div>
-        <p className="text-2xl tracking-wider sm:tracking-widest">Heading</p>
-        <p className="italic text-gray-400 text-xs sm:text-sm">SubHeading</p>
-        <p className="text-sm sm:text-lg">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
-          tenetur obcaecati, dignissimos iste, assumenda excepturi incidunt iure
-          ipsam molestiae quaerat, laborum inventore expedita! Sequi
-          reprehenderit itaque illo ea distinctio vel asperiores quia, culpa
-          fuga dolores corrupti magnam architecto nobis veniam obcaecati
-          quibusdam unde dignissimos aperiam ipsa? Eveniet dignissimos omnis
-          voluptates ad, quae qui eos quo, nam nobis suscipit iure modi.
+        <p className="text-2xl tracking-wider sm:tracking-widest">
+          {item?.title}
         </p>
+        <p className="italic text-gray-400 text-xs sm:text-sm">
+          {item?.journal?.name}
+        </p>
+        <p className="text-sm sm:text-lg">{item?.abstract}</p>
       </div>
       <div className="flex justify-end sm:justify-between">
         <div className="hidden sm:flex  gap-1 sm:gap-2 text-base">
@@ -58,18 +73,24 @@ const Data = ({ type }) => {
         <div className="flex gap-1 sm:gap-2">
           {type === "Academic" ? (
             <>
-              <Button variant="outlined" color="success">
-                Cite
-              </Button>
-              <Button variant="contained" color="success">
-                Explore
-              </Button>
+              <Link to={item?.url} target="_blank">
+                <Button variant="outlined" color="success">
+                  Cite
+                </Button>
+              </Link>
+              <Link to={item?.openAccessPdf?.url} target="_blank">
+                <Button variant="contained" color="success">
+                  Explore
+                </Button>
+              </Link>
             </>
           ) : (
             <>
-              <Button variant="contained" color="success">
-                Get Content
-              </Button>
+              <Link to={item.url} target="_blank">
+                <Button variant="contained" color="success">
+                  Get Content
+                </Button>
+              </Link>
             </>
           )}
         </div>
